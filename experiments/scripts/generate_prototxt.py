1a54c586db136e0f3a80f9776dc7bf28b82f7cd8
Mihai Morariu <m.a.morariu@delftrobotics.com>
2017-04-06 10:48:42 +0200

    Check if prototxt generation script is run from the working directory

--- experiments/scripts/generate_prototxt.py
+++ experiments/scripts/generate_prototxt.py
@@ -4,32 +4,36 @@ import yaml
 
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(
-  autoescape  = False,
-  loader      = FileSystemLoader(os.path.join("templates")),
-  trim_blocks = False
-)
+def renderTemplate(env, template_file, context):
+	return env.get_template(template_file).render(context)
 
-def renderTemplate(template_file, context):
-  return env.get_template(template_file).render(context)
-
-def createPrototxtFile(template_file, context, prototxt_file):
-  with open(prototxt_file, "w") as f:
-    f.write(renderTemplate(template_file, context))
+def createPrototxtFile(env, template_file, context, prototxt_file):
+	with open(prototxt_file, "w") as f:
+		f.write(renderTemplate(env, template_file, context))
 
 def readYamlFile(yaml_file):
-  with open(yaml_file, "r") as f:
-    return yaml.load(f)
+	with open(yaml_file, "r") as f:
+		return yaml.load(f)
 
 def run(yaml_file, template_file, prototxt_file):
-  context = readYamlFile(yaml_file)
-  createPrototxtFile(template_file, context, prototxt_file)
-  print("Saved to: '%s'" % prototxt_file)
+	env = Environment(
+		autoescape  = False,
+		loader      = FileSystemLoader("models/templates"),
+		trim_blocks = False
+	)
+
+	context = readYamlFile(yaml_file)
+	createPrototxtFile(env, template_file, context, prototxt_file)
+	print("Saved to: '%s'" % prototxt_file)
 
 if __name__ == "__main__":
-  if len(sys.argv) < 4:
-    print("Usage:", sys.argv[0], "[yaml_file] [template_file] [prototxt_file]")
-    sys.exit(-1)
+	if len(sys.argv) < 4:
+		print("Usage:", sys.argv[0], "[yaml_file] [template_file] [prototxt_file]")
+		sys.exit(-1)
 
-  run(sys.argv[1], sys.argv[2], sys.argv[3])
+	if not os.path.exists(os.path.join(os.getcwd(), "models/templates")):
+		print("Please run this script from the MNC working directory.")
+		sys.exit(-1)
+
+	run(sys.argv[1], sys.argv[2], sys.argv[3])
 
